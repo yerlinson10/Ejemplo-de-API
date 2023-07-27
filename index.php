39d665bed50ad6801ab818257d6a5c7548e249e9
Yerlinson Lora <yerlinsonlora@gmail.com>
2023-07-27 16:53:48 -0400

    Se actualizo el Endpoint de marcar, para que tambien sirva para desmarcar y se agrego un nuevo Endpoint para seleccionar solo uno.

--- index.php
+++ index.php
@@ -4,13 +4,33 @@ require 'flight/Flight.php';
 Flight::register('db', 'PDO', array('mysql:host=localhost;dbname=api', 'root', ''));
 
 Flight::route('/', function () {
-    echo 'hello world!';
+    echo 'Usa la tura tasks';
 });
 
 //Pedir lista
 Flight::route('GET /tasks', function () {
     try {
         $sentence = Flight::db()->prepare("SELECT * FROM `tasks`");
+        $sentence->execute();
+        $data = $sentence->fetchAll();
+        $response = [
+            "success" => true,
+            "tasks" => $data
+        ];
+    } catch (\Throwable $th) {
+        $response = [
+            "success" => false,
+            "message" => $th
+        ];
+    }
+
+    Flight::json($response);
+});
+
+//Pedir uno
+Flight::route('GET /tasks/@id', function ($id) {
+    try {
+        $sentence = Flight::db()->prepare("SELECT * FROM `tasks` WHERE id = $id");
         $sentence->execute();
         $data = $sentence->fetchAll();
         $response = [
@@ -77,17 +97,29 @@ Flight::route('DELETE /tasks/@id', function ($id) {
     Flight::json($response);
 });
 
-//Actualizar uno de lista
+//Marcar uno de lista
 Flight::route('PUT /tasks/@id', function ($id) {
     try {
-        $sql = "UPDATE `tasks` SET completed = 1 WHERE id = ?";
+        $sentence1 = Flight::db()->prepare("SELECT completed FROM `tasks` WHERE id = ?");
+        $sentence1->bindParam(1, $id);
+        $sentence1->execute();
+        $data = $sentence1->fetch();
+        if ($data["completed"] == 1) {
+            $completed = 0;
+            $message = "Tarea desmarcada como completada";
+        }else {
+             $completed = 1;
+             $message = "Tarea marcada como completada";
+        }
+           
+        $sql = "UPDATE `tasks` SET completed = $completed  WHERE id = ?";
         $sentence = Flight::db()->prepare($sql);
         $sentence->bindParam(1, $id);
         $sentence->execute();
 
         $response = [
             "success" => true,
-            "message" => "Tarea marcada como completada"
+            "message" => $message
         ];
     } catch (\Throwable $th) {
         $response = [
